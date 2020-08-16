3d30c79669ac91d97c356ed56f3c54e0df050032
NekoLogi <NekoLogi@github.com>
2020-08-16 06:29:36 +0200

    update

--- SyncAudio/MainWindow.xaml.cs
+++ SyncAudio/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,7 @@ namespace SyncAudio
         string localPath = "Songs/";
         int index = 0;
         bool songPlaying = false;
+        string[] duration;
 
         List<string> songList = new List<string>();
         MediaPlayer player = new MediaPlayer();
@@ -35,17 +37,20 @@ namespace SyncAudio
 
             Textbox1.Text += "**************************************\n";
 
+            
+            if (Keyboard.IsKeyDown(Key.P)) {
+                NextSong();
+            }
+            if (Keyboard.IsKeyDown(Key.Up) && player.Volume != 1.00) {
+                player.Volume += 0.05;
+            } else if (Keyboard.IsKeyDown(Key.Down) && player.Volume != 0) {
+                player.Volume -= 0.05;
+            }
             if (Keyboard.IsKeyDown(Key.Enter)) {
-                switch (songPlaying) {
-                    case true:
-                        player.Stop();
-                        break;
-                    case false:
-                        player.Play();
-                        break;
-                }
+                StartStopSong();
             }
         }
+
         public void FindSongs()
         {
             Textbox1.Clear();
@@ -59,7 +64,7 @@ namespace SyncAudio
 
                 foreach (var item in fileEntries) {
                     string[] song = item.Split('/', '.');
-                    if (song[2] == "wav") {
+                    if (song[2] == "wav" || song[2] == "mp3") {
                         songList.Add(song[1] + "." + song[2]);
                         Textbox1.Text += song[1] + "\n";
                     }
@@ -77,21 +82,46 @@ namespace SyncAudio
         public void StartStopSong()
         {
             switch (songPlaying) {
+                case false:
+                    player.Play();
+                    songPlaying = true;
+                    Textbox1.Clear();
+                    Task.Run(() => Counter());
+                    break;
                 case true:
                     player.Stop();
                     songPlaying = false;
                     FindSongs();
                     break;
-                case false:
-                    player.Play();
-                    songPlaying = true;
-                    Textbox1.Clear();
-                    Textbox1.Text += "Playing: " + songList[index];
-                    break;
             }
         }
-        public void StartSong()
+
+        public void Counter()
         {
+            try {
+                while (songPlaying == true) {
+                    Textbox1.Dispatcher.Invoke(() => {
+                        duration = player.Position.ToString().Split('.');
+                        Textbox1.Text = "Playing: " + songList[index] + "  |  " + duration[0];
+                    });
+                    Thread.Sleep(1000);
+                }
+            } catch (Exception e) {
+                Textbox1.Dispatcher.Invoke(() => {
+                    Textbox1.Text = e.ToString();
+                });
+                player.Dispatcher.Invoke(() => {
+                    player.Stop();
+                });
+            }
+        }
+
+        public void NextSong()
+        {
+            string[] parse = player.Position.TotalSeconds.ToString().Split(',');
+            int.TryParse(parse[0], out int seconds);
+            int.TryParse(player.Position.TotalMinutes.ToString(), out int minutes);
+            int.TryParse(player.Position.TotalHours.ToString(), out int hours);
             FindSongs();
             player.Stop();
 
@@ -101,13 +131,12 @@ namespace SyncAudio
                 index = 0;
             }
             Textbox1.Clear();
-            Textbox1.Text += "Playing: " + songList[index];
-
             player.Open(new Uri(path + songList[index]));
-            var time = new TimeSpan(0, 3, 0);
+            player.Position = new TimeSpan(hours, minutes, seconds);
             player.Play();
+            Task.Run(() => Counter());
         }
-
+        
         private void Start_Click(object sender, RoutedEventArgs e)
         {
             StartStopSong();
@@ -115,7 +144,14 @@ namespace SyncAudio
 
         private void Next_Click(object sender, RoutedEventArgs e)
         {
-            StartSong();
+            NextSong();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) {
+                StartStopSong();
+            }
         }
     }
 }
