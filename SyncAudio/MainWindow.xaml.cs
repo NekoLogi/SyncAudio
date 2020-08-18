d4dee3bb78028fe2c5d56cecd7813c7f7e33015c
NekoLogi <NekoLogi@github.com>
2020-08-18 19:44:49 +0200

    Release Update

--- SyncAudio/MainWindow.xaml.cs
+++ SyncAudio/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -19,12 +20,15 @@ namespace SyncAudio
 {
     public partial class MainWindow : Window
     {
-
-        string path = "file://C:/Songs/";
+        string path = ("file://" + System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/Songs/").Replace('\\', '/');
         string localPath = "Songs/";
         int index = 0;
         bool songPlaying = false;
+        bool songPaused = false;
         string[] duration;
+        int hours, minutes, seconds;
+        double volumeSave = 0.3;
+        bool trackMode = false;
 
         List<string> songList = new List<string>();
         MediaPlayer player = new MediaPlayer();
@@ -34,21 +38,11 @@ namespace SyncAudio
             InitializeComponent();
             FindSongs();
             player.Open(new Uri(path + songList[index]));
-
+            slider.Value = 1;
+            player.Volume = slider.Value;
+            volumeName.Content = CalculateVolume().ToString() + "%";
             Textbox1.Text += "**************************************\n";
-
-            
-            if (Keyboard.IsKeyDown(Key.P)) {
-                NextSong();
-            }
-            if (Keyboard.IsKeyDown(Key.Up) && player.Volume != 1.00) {
-                player.Volume += 0.05;
-            } else if (Keyboard.IsKeyDown(Key.Down) && player.Volume != 0) {
-                player.Volume -= 0.05;
-            }
-            if (Keyboard.IsKeyDown(Key.Enter)) {
-                StartStopSong();
-            }
+            ChangeTrackMode();
         }
 
         public void FindSongs()
@@ -79,16 +73,19 @@ namespace SyncAudio
                 Textbox1.Text += "Error: Can't find 'Songs' folder.\n";
             }
         }
+
         public void StartStopSong()
         {
             switch (songPlaying) {
                 case false:
+                    Start.Content = "Stop";
                     player.Play();
                     songPlaying = true;
                     Textbox1.Clear();
                     Task.Run(() => Counter());
                     break;
                 case true:
+                    Start.Content = "Start";
                     player.Stop();
                     songPlaying = false;
                     FindSongs();
@@ -102,9 +99,13 @@ namespace SyncAudio
                 while (songPlaying == true) {
                     Textbox1.Dispatcher.Invoke(() => {
                         duration = player.Position.ToString().Split('.');
-                        Textbox1.Text = "Playing: " + songList[index] + "  |  " + duration[0];
+                        if (songPaused == true) {
+                            Textbox1.Text = "Playing: " + songList[index] + "  |  " + duration[0] + "  |  " + "Paused";
+                        } else {
+                            Textbox1.Text = "Playing: " + songList[index] + "  |  " + duration[0];
+                        }
                     });
-                    Thread.Sleep(1000);
+                    Thread.Sleep(200);
                 }
             } catch (Exception e) {
                 Textbox1.Dispatcher.Invoke(() => {
@@ -116,12 +117,37 @@ namespace SyncAudio
             }
         }
 
+        public void CalculateTime()
+        {
+            if (trackMode == true) {
+                string[] parse = player.Position.TotalSeconds.ToString().Split(',');
+                int.TryParse(parse[0], out seconds);
+                int.TryParse(player.Position.TotalMinutes.ToString(), out minutes);
+                int.TryParse(player.Position.TotalHours.ToString(), out hours);
+            } else if (trackMode == false) {
+                hours = 0;
+                minutes = 0;
+                seconds = 0;
+            }
+        }
+
+        public void ChangeTrackMode()
+        {
+            if (trackMode == true) {
+                trackState.Content = "Off";
+                trackState.Foreground = Brushes.Red;
+                trackMode = false;
+            } else if (trackMode == false) {
+                trackState.Content = "On";
+                trackState.Foreground = Brushes.Green;
+                trackMode = true;
+            }
+        }
+
         public void NextSong()
         {
-            string[] parse = player.Position.TotalSeconds.ToString().Split(',');
-            int.TryParse(parse[0], out int seconds);
-            int.TryParse(player.Position.TotalMinutes.ToString(), out int minutes);
-            int.TryParse(player.Position.TotalHours.ToString(), out int hours);
+            CalculateTime();
+
             FindSongs();
             player.Stop();
 
@@ -133,13 +159,91 @@ namespace SyncAudio
             Textbox1.Clear();
             player.Open(new Uri(path + songList[index]));
             player.Position = new TimeSpan(hours, minutes, seconds);
+            songPlaying = true;
+            songPaused = false;
+            Start.Content = "Stop";
             player.Play();
             Task.Run(() => Counter());
         }
-        
+
+        public void PrevSong()
+        {
+            CalculateTime();
+
+            FindSongs();
+            player.Stop();
+
+            if (index != 0) {
+                index--;
+            } else {
+                index = songList.Count - 1;
+            }
+            Textbox1.Clear();
+            player.Open(new Uri(path + songList[index]));
+            player.Position = new TimeSpan(hours, minutes, seconds);
+            songPlaying = true;
+            songPaused = false;
+            Start.Content = "Stop";
+            player.Play();
+            Task.Run(() => Counter());
+        }
+
+        public int CalculateVolume()
+        {
+            return Convert.ToInt32(slider.Value / 1 * 100);
+        }
+
+        public void PauseSong()
+        {
+            if (songPaused == false) {
+                songPaused = true;
+                songPlaying = true;
+                Pause.Content = "Play";
+                player.Pause();
+
+            } else {
+                songPaused = false;
+                songPlaying = false;
+                Pause.Content = "Pause";
+                player.Play();
+            }
+        }
+
         private void Start_Click(object sender, RoutedEventArgs e)
         {
             StartStopSong();
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            volumeName.Content = CalculateVolume().ToString() + "%";
+            player.Volume = slider.Value;
+        }
+
+        private void volumeName_Click(object sender, RoutedEventArgs e)
+        {
+            if (slider.Value != 0) {
+                volumeSave = slider.Value;
+                slider.Value = 0;
+                volumeName.Content = "Mute";
+            } else {
+                slider.Value = volumeSave;
+            }
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            PauseSong();
+        }
+
+        private void Prev_Click(object sender, RoutedEventArgs e)
+        {
+            PrevSong();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Shortcuts:\n\nEnter = Play/Stop Player\nSpace = Pause Track\n\nArrow Right = next Track\nArrow Left = Previous Track\n\nArrow Up = Volume Up\nArrow Down = Volume Down\n\nM = Change Track Mode\nOff = New Track begins from beginning\nOn = New Track begins where the Previous ended");
         }
 
         private void Next_Click(object sender, RoutedEventArgs e)
@@ -147,10 +251,28 @@ namespace SyncAudio
             NextSong();
         }
 
-        private void Window_KeyDown(object sender, KeyEventArgs e)
+        private void Window_KeyDown_1(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter) {
                 StartStopSong();
+            } else if (e.Key == Key.Space) {
+                PauseSong();
+            }
+
+            if (e.Key == Key.Up && player.Volume != 1.00) {
+                player.Volume += 0.05;
+            } else if (e.Key == Key.Down && player.Volume != 0) {
+                player.Volume -= 0.05;
+            }
+
+            if (e.Key == Key.Right) {
+                NextSong();
+            } else if (e.Key == Key.Left) {
+                PrevSong();
+            }
+
+            if (e.Key == Key.M) {
+                ChangeTrackMode();
             }
         }
     }
